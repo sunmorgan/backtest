68a4e500420b3b151428e14fea29e69ae0278091
Morgan Sun <sunmorgan97@gmail.com>
2024-11-26 19:49:44 -0500

    general csv and yahoo

--- feeds/csvfeeds.py
+++ feeds/csvfeeds.py
@@ -1,1 +1,14 @@
-fdsf:
+import pandas as pd
+
+class CSVFeeds:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self._data = None
+        self._load_data()
+
+    def _load_data(self):
+        self._df = pd.read_csv(self.file_path)
+        self._data = self._df.to_string() 
+
+    def get_data(self):
+        return self._data
