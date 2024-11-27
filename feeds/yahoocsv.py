fb21247cbb0c909f2677c0eda951d52ca5527fa0
Morgan Sun <sunmorgan97@gmail.com>
2024-11-26 23:07:36 -0500

    update multiple data suppliers

--- feeds/yahoocsv.py
+++ feeds/yahoocsv.py
@@ -1,2 +1,13 @@
+import pandas as pd 
+
 class YahooCSV:
-    
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self._data = None
+        self._process()
+
+    def _process(self):
+        self.df = pd.read_csv(file_path) 
+
+    def get_data(self):
+        pass
