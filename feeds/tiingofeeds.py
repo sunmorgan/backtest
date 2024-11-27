9261e36031372231b84fd492f4841cfa826e9ccc
Morgan Sun <sunmorgan97@gmail.com>
2024-11-27 17:46:07 -0500

    write csv from http request

--- feeds/tiingofeeds.py
+++ feeds/tiingofeeds.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import requests
+import csv 
 
 class TiingoCSV:
     def __init__(self, api_key):
@@ -10,4 +11,30 @@ class TiingoCSV:
                 'Content-Type': 'application/json'
                 } 
         response = requests.get(f"https://api.tiingo.com/tiingo/daily/{symbol.lower()}/prices?startDate={start_date}&token={self._api_key}", headers=self.headers)
-        print(response.json())
+        csv_name = f"{symbol}-{start_date}"
+        
+        with open(csv_name, 'w', newline='') as csvfile:
+            fieldnames = ['date', 'open', 'high', 'low', 'close', 'volume', 'adjOpen', 'adjHigh', 'adjLow',
+                          'adjClose', 'adjVolume', 'dividend', 'split']
+            
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            
+            writer.writeheader()
+            for r in response:
+                date = r['date']
+                openn = r['open']
+                high = r['high']
+                low = r['low']
+                close = r['close']
+                volume = r['volume']
+                adj_open = r['adjOpen']
+                adj_high = r['adjHigh']
+                adj_low = r['adjLow']
+                adj_close = r['adjClose']
+                adj_volume = r['adjVolume']
+                dividend = r['divCash']
+                split = r['splitFactor']
+
+                writer.writerow({'date': date, 'open': openn, 'high': high, 'low': low,
+                                 'close': close, 'volume': volume, 'adjOpen': adj_open, 'adjHigh': adj_high,
+                                 'adjLow': adj_low, 'adjVolume': adj_volume, 'dividend': dividend, 'split': split})
